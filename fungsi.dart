5615cb818baecf427054894e5f570d2962a18cee
Ikhwanudin Gifari <100889069+Gifari28@users.noreply.github.com>
2022-07-25 23:37:46 +0700

    Add files via upload

---
+++ fungsi.dart
@@ -0,0 +1,23 @@
+void main() {
+  // sebuah fungsi metode untuk pengelompokan yang di eksekusi pada code tertentu
+  /* tipe namaFungsi(parameter) {
+    kode
+    kode
+    return nilai 9sesuai tipe)
+    */
+
+  String nama = 'Ikhwanudin gifari';
+  perkenalan(nama);
+
+  int sisi = 10;
+  int volume = volumekubus(sisi);
+  print(volume);
+}
+
+void perkenalan(String nama) {
+  print('Halo, Nama saya $nama');
+}
+
+int volumekubus(int sisi) {
+  return sisi = sisi * sisi * sisi;
+}
